134060b10329e9fcbe870da3e1daaf1b092ce417
Marwan-Qasim <markzlopp@gmail.com>
2026-02-27 00:03:59 +0530

    updated styling and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
 import React, {useState, useEffect} from 'react'
+import styled from 'styled-components';
 import Login from './components/login';
+
+const AppContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+  background-color: var(--black);
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+  margin-left: var(--nav-width);
+  padding: 0;
+  min-height: 100vh;
+`;
+
 
 function App() {
  
   return (
     <>
-      <Login></Login>
+      <AppContainer>
+        <MainContent>
+          <Login></Login>
+        </MainContent>
+      </AppContainer>
     </>
   )
 }
